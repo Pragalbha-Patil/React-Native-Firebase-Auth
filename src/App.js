cd710124feed04b293cef79da5c88727a2dd68d1
Pragalbha Patil <pragalbha.p@somaiya.edu>
2020-03-27 19:27:32 +0530

    React Native Authentication App Init
    All the reusable components has been imported from previous project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Text, Component} from 'react-native';
+
+class App extends Component {
+  render() {
+    return (
+      // eslint-disable-next-line react/jsx-no-undef
+      <View>
+        <Text>Hello from App</Text>
+      </View>
+    );
+  }
+}
+
+export default App;
